0067dc5a3374d39ab076312a2e0f68e57f037a9d
Semenchenko Andriy <semandr.code@gmail.com>
2020-09-18 22:12:00 +0200

    adding first function of adding operation

---
+++ assets/scripts/app.js
@@ -0,0 +1,11 @@
+let currentResult = 0
+
+function add(x, y) {
+  return x + y
+}
+
+currentResult = add(1, 2)
+
+let calculationDescription = `(0 + 10) * 3 / 2 - 1`
+
+outputResult(currentResult, calculationDescription)
