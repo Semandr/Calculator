73c1cf0fb311ea0c53bde23989f6803b8884e07d
Semenchenko Andriy <semandr.code@gmail.com>
2020-09-19 11:49:46 +0200

    add eventLinsteners for functions of -,+,/,*.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -37,3 +37,6 @@ function divide() {
   createAndWriteOutput('/', initialResult, enteredNumber)
 }
 addBtn.addEventListener('click', add)
+subtractBtn.addEventListener('click', subtract)
+multiplyBtn.addEventListener('click', multiply)
+divideBtn.addEventListener('click', divide)
