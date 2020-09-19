45bdca5111d537d2dda70344c4bc390265c7057e
Semenchenko Andriy <semandr.code@gmail.com>
2020-09-19 17:29:24 +0200

    create function writeToLog and apply in all operations functions of calculator

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -11,42 +11,55 @@ function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
   outputResult(currentResult, calcDescription)
 }
 
+function writeToLog(
+  operationIdentifier,
+  prevResult,
+  operationNumber,
+  newResult
+) {
+  const logEntry = {
+    operation: operationIdentifier,
+    prevResult: prevResult,
+    number: operationNumber,
+    result: newResult,
+  }
+  logEntries.push(logEntry)
+  console.log(logEntries)
+}
+
 function add() {
   const enteredNumber = getUserInput()
   const initialResult = currentResult
   currentResult += parseInt(userInput.value)
-  createAndWriteOutput('+', initialResult, enteredNumber)
-  const logEntry = {
-    operation: 'ADD',
-    prevResult: initialResult,
-    number: enteredNumber,
-    result: currentResult
-  }
-  logEntries.push(logEntry)
-  console.log(logEntries)
+  createAndWriteOutput("+", initialResult, enteredNumber)
+  writeToLog('ADD', initialResult, enteredNumber, currentResult)
 }
 
 function subtract() {
   const enteredNumber = getUserInput()
   const initialResult = currentResult
   currentResult -= parseInt(userInput.value)
-  createAndWriteOutput('-', initialResult, enteredNumber)
+  createAndWriteOutput("-", initialResult, enteredNumber)
+  writeToLog('SUBTRACT', initialResult, enteredNumber, currentResult)
 }
 
 function multiply() {
   const enteredNumber = getUserInput()
   const initialResult = currentResult
   currentResult *= parseInt(userInput.value)
-  createAndWriteOutput('*', initialResult, enteredNumber)
+  createAndWriteOutput("*", initialResult, enteredNumber)
+  writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult)
 }
 
 function divide() {
   const enteredNumber = getUserInput()
   const initialResult = currentResult
   currentResult /= parseInt(userInput.value)
-  createAndWriteOutput('/', initialResult, enteredNumber)
+  createAndWriteOutput("/", initialResult, enteredNumber)
+  writeToLog('DIVIDE', initialResult, enteredNumber, currentResult)
 }
-addBtn.addEventListener('click', add)
-subtractBtn.addEventListener('click', subtract)
-multiplyBtn.addEventListener('click', multiply)
-divideBtn.addEventListener('click', divide)
+
+addBtn.addEventListener("click", add)
+subtractBtn.addEventListener("click", subtract)
+multiplyBtn.addEventListener("click", multiply)
+divideBtn.addEventListener("click", divide)
